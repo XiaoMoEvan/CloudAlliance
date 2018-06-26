a1f9d86c2859fde1f9b0cfa6c472583937715175
二颜 <1431128779@qq.com>
2018-06-26 14:32:34 +0800

    ,Modify the navigation style

--- js/Index/index.js
+++ js/Index/index.js
@@ -1,7 +1,12 @@
 $(function() {
     //var index = layer.load(2, { shade: false }); //0代表加载的风格，支持0-2
-
-    //$('#app').fullpage(); //初始化 fullPage
+    var _fullpageOption = {
+        loopBottom: true,
+        keyboardScrolling: false,
+        anchors: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6'],
+        menu: '#cloud-nav-menu'
+    };
+    $('#cloud-container').fullpage(_fullpageOption); //初始化 fullPage
     var wow = new WOW({
         boxClass: 'wow',
         animateClass: 'animated',
@@ -29,7 +34,7 @@ $(function() {
             success: function(layero) {
                 var btn = layero.find('.layui-layer-btn');
                 btn.find('.layui-layer-btn0').attr({
-                    href: 'http://www.925i.cn',
+                    href: 'http://weibo.com/tzxiaomo56',
                     target: '_blank'
                 });
             }
@@ -51,10 +56,10 @@ $(function() {
         });
     })
 
-    $(".navLink").click(function(e) {
-        e.preventDefault();
-        $(this).addClass("navActive").parent().siblings().find("a").removeClass("navActive");
-    })
+    // $(".navLink").click(function(e) {
+    //     //e.preventDefault(); //阻止了表单提交 会影响fullpage绑定菜单
+    //     $(this).parent().addClass("active").siblings().removeClass("active");
+    // })
 
     $(".navLink").hover(function(e) {
         e.preventDefault();
@@ -66,16 +71,51 @@ $(function() {
             "width": $(this).width(),
             "left": $prv.length == 0 ? 0 : $prv.width() * _index
         })
-    }, function() {
+    })
+    $("#cloud-nav-menu").mouseleave(function() {
+        resetNavBLinePostion(this);
+    })
+
+    function resetNavBLinePostion(ele) {
         var $navBLine = $(".nav-bottom-line");
         $navBLine.hide();
-    })
+
+        //1.获取当前激活的位置     
+        var _activePostion = $(".active").index();
+        var _toLeft = 0;
+        for (var i = 0; i < _activePostion; i++) {
+            _toLeft += $(ele).find("li").eq(i).width();
+        }
+        //2.重置线的位置
+        $navBLine.css({
+            "left": _toLeft
+        });
+    }
+
+    function restIndexArea() {
+        var _$indexArea = $(".indexArea");
+        var _marginTopH = Number((312 / 2) - (_$indexArea.height() / 2));
+        _$indexArea.css({
+            "margin-top": _marginTopH + "px"
+        })
+    }
 
     function navLinkLineInit() {
         var $navLink = $(".head-nav").find(".navLink").eq(0);
+        var $navBLine = $(".nav-bottom-line");
         var _w = $navLink.width();
-        $navLink.addClass("navActive");
-        $(".nav-bottom-line").width(_w);
+        //$navLink.addClass("navActive");
+        //$navBLine.width(_w);
+        //$navBLine.removeClass("cloud-displayNone");
     }
-    navLinkLineInit();
+    window.onload = function() {
+        navLinkLineInit();
+        //restIndexArea();
+        $(".indexArea").removeClass("cloud-displayNone");
+        resetNavBLinePostion($("#cloud-nav-menu"));
+
+    }
+    window.resize = function() {
+        //restIndexArea();
+    }
 })
